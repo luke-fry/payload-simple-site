bca59cbb92a0628a68fbc7ce7ec4486b481aba53
Luke Fry <luke.fry096@gmail.com>
2026-01-19 22:05:35 +1000

    Fix: Use MONGODB_URI environment variable

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -59,7 +59,7 @@ export default buildConfig({
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: mongooseAdapter({
-    url: process.env.DATABASE_URL || '',
+    url: process.env.MONGODB_URI || process.env.DATABASE_URL || '',
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
